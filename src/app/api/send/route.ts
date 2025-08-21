f4c747485fd2c4f923f7832d692ac6133be1c4db
Bhavishy Jain <70500535+bhavishyjain@users.noreply.github.com>
2025-08-21 19:41:42 +0530

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,6 +2,7 @@ import { EmailTemplate } from "@/components/email-template";
 import { config } from "@/data/config";
 import { Resend } from "resend";
 import { z } from "zod";
+export const runtime = 'edge';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
